b87c07f37dc6d2c26fac0ca4eecc88d6bbab558b
SergeyDedikov <serded59@yandex.ru>
2022-05-16 14:42:47 +0500

    feat: files structure

---
+++ src/utils/const.js
@@ -0,0 +1,16 @@
+const regionCodes = [
+  {
+    region: "Россия",
+    code: "+7",
+  },
+  {
+    region: "Китай",
+    code: "+86",
+  },
+  {
+    region: "Перу",
+    code: "+51",
+  },
+];
+
+export { regionCodes };
