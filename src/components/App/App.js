433c22c0f9ac3bd317010c04f9e6e9699baa078a
SergeyDedikov <serded59@yandex.ru>
2022-05-18 16:41:54 +0500

    feat: first loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import Form from "../Form/Form";
 import Section from "../Section/Section";
 import List from "../List/List";
+import api from "../../utils/apiPhones";
 
 export default function App() {
+  const [onLoad, setOnLoad] = useState(false);
   const [numbersList, setNumbersList] = useState([]);
+
+  // -- Первоначальный рендер
+  useEffect(() => {
+    api
+      .deleteMovie()
+      .then((res) => {
+        setOnLoad(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   function onSendData(newNumber) {
     setNumbersList([newNumber, ...numbersList]);
   }
-  return (
+
+  return onLoad ? (
     <>
       <Section name="form">
         <Form onSendData={onSendData} />
@@ -20,5 +33,5 @@ export default function App() {
         <List numbersList={numbersList} />
       </Section>
     </>
-  );
+  ) : null;
 }
